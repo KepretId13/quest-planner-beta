be350a81cfacf99d6be8154c4b6bb60610ddb636
AsGe330 <85639860+KepretId13@users.noreply.github.com>
2026-03-19 08:40:33 +0700

    script.js

--- script.js
+++ script.js
@@ -12,5 +12,15 @@ function nextStep(current, next){
       nextEL.classList.add("active");
     },10);
 
+    // hasil akhir
+    if(next === 5){
+      document.getElementById("result").innerText = generateResult();
+    }
+
   }, 300);
+}
+
+function generateResult(){
+  let list = ["Peaceful", "Easy", "Normal", "Hard", "Insane"];
+  return list[Math.floor(Math.random()*list.length)];
 }
